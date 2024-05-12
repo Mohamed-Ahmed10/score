c5f7afc33b92497f366b35b38a88245c4b256495
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2024-05-12 23:15:13 +0300

    Edits

--- src/views/Climbing.jsx
+++ src/views/Climbing.jsx
@@ -1,7 +1,7 @@
 export default function Climbing() {
     return (
         <div className="text-center mt-4 pt-4">
-            <h1 className="text-primary">climbing VR</h1>
+            <h1 >climbing VR</h1>
             <p className="text-white w-50 m-auto mt-4"> Within the climbing VR game, change management competencies can be assessed through adaptability to varying challenges, strategic decision-making in utilizing resources efficiently (like energy shakes), time management skills to reach multiple stops within a set timeframe, resilience in facing setbacks or fatigue, and effective communication in coordinating movements (like using both hands). Additionally, problem-solving abilities and teamwork may be evaluated as players strategize and collaborate to overcome obstacles collectively.</p>
         </div >
     )
--- src/views/Tennis.jsx
+++ src/views/Tennis.jsx
@@ -1,7 +1,7 @@
 export default function Tennis() {
     return (
         <div className="text-center mt-4 pt-4">
-            <h1 className="text-primary">Tennis</h1>
+            <h1>Tennis</h1>
             <p className="text-white w-50 m-auto mt-4">In the tennis game, change management competencies are evident through adaptability, problem-solving, and flexibility as players navigate equipment variations mid-match. Rapid learning and decision-making under pressure showcase the importance of continuous adaptation and strategic agility. Moreover, emphasizing change management planning underscores the need for proactive strategies to facilitate smooth transitions amidst uncertainty.</p>
         </div>
     )
