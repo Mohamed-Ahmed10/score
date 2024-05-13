8f0ef42956e7709d35ab5e6d1ecda7698ea3af99
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2024-05-13 03:36:32 +0300

    Edit image

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo_light.svg"
 export default function Home() {
     return (
         <div className="w-25 mx-auto mt-4 pt-4">
-            <img className="img-fluid" src={logo} alt="Logo" />
+            <img className="w-100" src={logo} alt="Logo" />
         </div>
     )
 }
